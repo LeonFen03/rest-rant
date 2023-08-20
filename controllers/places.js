87f4cf3286a290ec6cab918c73a67daa3e657b2d
LeonFen03 <LeonFen03@gmail.com>
2023-08-20 10:02:02 -0400

    Fixed Router Declaration

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
 
-module.exports = router;
+const router = require('express').Router();
 
 router.get('/',(req,res)=> {
     res.send('GET /places');
 })
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express')
-
 require('dotenv').config();
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
+app.use('/spacees', require('./controllers/places'))
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404</h1>')
