0fd5c5a460916b5a61d4e3db3ad744e3de6d74c3
LeonFen03 <LeonFen03@gmail.com>
2023-08-29 21:30:26 -0400

    Adding home page view

--- controllers/places.js
+++ controllers/places.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 router.get('/',(req,res)=> {
     res.render('places/index');
 })
-
+router.get('/:id/edit', (req, res) => {
+    res.render('places/edit')
+  })
+  
 module.exports = router;
